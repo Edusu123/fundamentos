5c6e92e700fea65edcde362327261436125a2d8d
Edusu123 <eduardo.c.pires@outlook.com>
2023-08-04 15:28:20 -0300

    Entendendo o JSX.

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+export default function Inicio() {
+  return (
+    <div>
+      <h1>Fundamentos de Next.JS & React</h1>
+      <h2>
+        Estudando o <i>framework</i>
+      </h2>
+    </div>
+  );
+}
