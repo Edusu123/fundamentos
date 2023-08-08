2102e46c82449a84cdc82fa2ece054d9a3ddab2a
Edusu123 <eduardo.c.pires@outlook.com>
2023-08-08 15:37:13 -0300

    Componente Layout

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
-import styles from "../styles/estiloso.module.css";
+import Layout from "@/components/Layout"
+import styles from "../styles/Estiloso.module.css"
 
 export default function Estiloso() {
   return (
-    <div className={styles.roxo}>
-      <Link href="/">Voltar</Link>
-      <h1>Estilo usando CSS Módulos</h1>
-    </div>
+    <Layout titulo="Exemplo de CSS modularizado">
+      <div className={styles.roxo}>
+        <h1>Estilo usando CSS Módulos</h1>
+      </div>
+    </Layout>
   );
 }
