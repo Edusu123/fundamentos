37391277434f4993846862489cd9c329125b2051
Edusu123 <eduardo.c.pires@outlook.com>
2023-08-15 16:03:25 -0300

    Usando API #02

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,8 @@
 export default function handler(req, res) {
   res.status(200).json({
     name: 'Test API',
-    metodo: req.method
+    metodo: req.method,
+    nome: req.query.nome,
+    idade: req.query.idade
   })
 }
